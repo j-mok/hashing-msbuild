70be5769c9ac617fdb0c943f484c4cd0a01188f2
Jacek Mokrzycki <jmokrzyc@gmail.com>
2017-01-06 12:29:07 +0100

    Add a project with a timestamp-rewinding task stub and link it to the test project.

---
+++ IncrementalBuildPoC/RewindTimestampsTask/RewindTimestamps.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Build.Utilities;
+using Microsoft.Build.Framework;
+
+namespace HashingMSBuild
+{
+    public class RewindTimestamps : Task
+    {
+        public override bool Execute()
+        {
+            foreach (var item in SourceFiles)
+            {
+                Log.LogMessage(MessageImportance.High, "Source file: {0}", item.ItemSpec);
+            }
+
+            return true;
+        }
+
+        [Required]
+        public ITaskItem[] SourceFiles { get; set; }
+    }
+}
